f9b5525060dc3dcd0787ad486e8130a80a63c25f
Josephine Holst-Christen <joho23ak@student.cbs.dk>
2025-09-23 16:42:56 +0200

    Log du knap

--- Queless/components/HomeComponent.js
+++ Queless/components/HomeComponent.js
@@ -1,13 +1,20 @@
-// Josephine Holst-Christensen
+//Josephine Holst-Christensen
 import * as React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, Button, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from '../style/home.styles';
 
 export default function HomeContent() {
+  const navigation = useNavigation();
+
   return (
     <ScrollView style={styles.page} contentContainerStyle={styles.container}>
       <Text style={styles.title}>Hjemmeskærm</Text>
       <Text style={styles.body}>Brug fanerne i bunden for at navigere.</Text>
+      <View style={{ marginTop: 16 }}>
+        <Button title="Log ud" onPress={() => navigation.navigate('Login')} />
+        {/* En knap der navigere tilbage til login siden :) */}
+      </View>
     </ScrollView>
   );
 }
