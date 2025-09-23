cd6aa2ac67520febe2da9ae8afab3de0f9497b1e
Malou Luthcke <malu23ag@student.cbs.dk>
2025-09-23 14:13:44 +0200

    changes

--- Queless/App.js
+++ Queless/App.js
@@ -1,19 +1,8 @@
-// App.js
-import * as React from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 
-// Din tab-navigator ligger nu her:
+// tab-navigationen 
 import Home from './screens/Home';
 
 const Stack = createNativeStackNavigator();
@@ -22,11 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ title: 'Log ind' }}
-        />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         {/* Home er din bundmenu fra screens/Home.js */}
         <Stack.Screen
           name="Home"
@@ -38,17 +23,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center', padding: 24 },
-  title: { fontSize: 24, marginBottom: 16 },
-  input: {
-    width: '100%',
-    maxWidth: 360,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    paddingHorizontal: 12,
-    paddingVertical: 10,
-    marginBottom: 12,
-  },
-});
+
